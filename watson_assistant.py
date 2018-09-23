f448ecf54b402fd7eacc675afeaa05dfac953836
Ilkay Tevfik Devran <devrani@mef.edu.tr>
2018-09-23 16:49:19 +0300

    Final Version So Far

--- watson_assistant.py
+++ watson_assistant.py
@@ -2,9 +2,9 @@
 
 from watson_developer_cloud import AssistantV1, WatsonApiException
 import json, ast
-#from watson_developer_cloud import LanguageTranslatorV2 as LanguageTranslator
+from watson_developer_cloud import LanguageTranslatorV3 as LanguageTranslator
 
-def get_response_from_watson_assistamt(url, username, password, version, workspace_id):
+def get_response_from_watson_assistamt(version, username, password, url, workspace_id):
 
     print "Enter your text here"
     inpt = raw_input('>>>')
@@ -38,49 +38,51 @@ def get_response_from_watson_assistamt(url, username, password, version, workspa
     except WatsonApiException as ex:
         return "Method failed with status code " + str(ex.code) + ": " + ex.message
 
-def get_response_from_language_translator(url, username, password):
-    """
+def get_response_from_language_translator(version, iam_apikey, url, word):
+
     language_translator = LanguageTranslator(
-    username=username,
-        password=password,
-        url=url
+        version = version,
+        iam_apikey = iam_apikey,
+        url= url
     )
+
     try:
         translation = language_translator.translate(
-        text='Hello',
-        model_id='en-es')
-        print(json.dumps(translation, indent=2, ensure_ascii=False))
+            text=str(word),
+            model_id='tr-en').get_result()
+        #print(json.dumps(translation, indent=2, ensure_ascii=False))
 
         # Returned answer string manupulation
-        j = json.dumps(response, indent=2)
+        j = json.dumps(translation, indent=2)
         out_json = json.loads(j)
-        output = out_json['output']['text'][0]  # *** check here ***
+        output = out_json['translations'][0]['translation']  # *** check here ***
         
         return output
 
     except WatsonApiException as ex:
         return "Method failed with status code " + str(ex.code) + ": " + ex.message
 
-    """
-    return 0
-
-
-
 def main():
     # Necessary credentials
-    version= <'version'>
+    
+    # Watson Assistant Parameters
+    assistant_version = <'version'>
     username = <'username'>
     password = <'password'>
-    url = <'url'>
+    assistant_url = <'url'>
     workspace_id= <'workspace_id'>
+
+    # Watson LanguageTranslator Parameters
+    tranlator_version = = <'version'>
+    iam_apikey = <'iam_apikey'>
+    translator_url = <'url'>
     
-    
+
     # Send input text to watson conversation api and get the response of assistant
-    assistant_response = get_response_from_watson_assistamt(url, username, password, version, workspace_id)
-    print(assistant_response)
-    
+    assistant_response = get_response_from_watson_assistamt(assistant_version, username, password, assistant_url, workspace_id).encode("utf-8")
+
     # Send response from watson assistan to language translator and get response of it
-    translator_response = get_response_from_language_translator(url, username, password)
+    translator_response = get_response_from_language_translator(translator_version, iam_apikey, translator_url, assistant_response)
     print(translator_response)
     
 
